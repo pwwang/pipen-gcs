b3f2985fe4641edff2821b1618889e25dca60966
pwwang <pwwang@pwwang.com>
2026-08-05 21:20:42 -0700

    chore: migrate to uv

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 This will not be included in the distribution.
-The distribution is managed by poetry
-This file is kept only for
+The distribution is managed by uv and setuptools.
+This file is kept only for:
 1. Github to index the dependents
 2. pip install -e .
 
 Do NOT use this to install this package, unless you handled the dependencies
-by your self:
+by yourself:
 
 pip install git+https://...
 """
